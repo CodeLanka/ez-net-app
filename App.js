eb00bc72713e7fe6619f2c08930c0e2594ca95bf
Anusha Dharmasena <nushydude@gmail.com>
2017-07-05 20:47:06 +1000

    Fix indentation

--- App.js
+++ App.js
@@ -1,64 +1,58 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  TouchableHighlight
-} from 'react-native';
+import { StyleSheet, View, TouchableHighlight } from 'react-native';
 import { Container, Content } from 'native-base';
 import ResponsiveImage from 'react-native-responsive-image';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   layout: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   box: {
     padding: 20,
     backgroundColor: 'rgba(0,0,0,0.05)',
-    margin: 10,
-  },
+    margin: 10
+  }
 });
 
 export default class ReactNativeProject extends Component {
 
-    static navigationOptions = {
-        title: 'අන්තර්ජාල නැබ',
-    };
+  static navigationOptions = {
+    title: 'අන්තර්ජාල නැබ'
+  };
 
-    render() {
-        const {navigate} = this.props.navigation;
-        return (
-          <Container>
-              <Content>
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Container>
+        <Content>
 
-                <View style={[styles.layout]}>
-                    <TouchableHighlight onPress={() => navigate('Detail', { title: 'පුවත් පත්' })}>
-                        <View style={styles.box} >
-                            <ResponsiveImage source={require('./img/news-papers.png')} initWidth="138" initHeight="138"/>
-                        </View>
-                    </TouchableHighlight>
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                </View>
+          <View style={[styles.layout]}>
+            <TouchableHighlight onPress={() => navigate('Detail', { title: 'පුවත් පත්' })}>
+              <View style={styles.box}>
+                <ResponsiveImage source={require('./img/news-papers.png')} initWidth="138" initHeight="138" />
+              </View>
+            </TouchableHighlight>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138" />
+            </View>
+          </View>
 
-                <View style={[styles.layout]}>
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                </View>
-              </Content>
-          </Container>
-        );
-
-
-    }
+          <View style={[styles.layout]}>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138" />
+            </View>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/unknown.png')} initWidth="138" initHeight="138" />
+            </View>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
 }
--- DetailScreen.js
+++ DetailScreen.js
@@ -1,74 +1,62 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Linking,
-  TouchableHighlight,
-} from 'react-native';
+import { StyleSheet, View, Linking, TouchableHighlight } from 'react-native';
 import { Container, Content } from 'native-base';
 import ResponsiveImage from 'react-native-responsive-image';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   layout: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   box: {
     padding: 20,
     backgroundColor: 'rgba(0,0,0,0.05)',
-    margin: 10,
-  },
+    margin: 10
+  }
 });
 
 export default class DetailScreen extends Component {
 
-    // Function onButtonPress
-    _onPressButton(url){
-            //Alert.alert("Lol");
-            const uri = url;
-            Linking.openURL(uri).catch(err => console.error('An error occurred', err));
-        }
+  // Function onButtonPress
+  _onPressButton(url) {
+    const uri = url;
+    Linking.openURL(uri).catch(err => console.error('An error occurred', err));
+  }
 
-    static navigationOptions = ({ navigation }) => ({
-        title: `${navigation.state.params.title}`,
-    });
+  static navigationOptions = ({ navigation }) => ({ title: `${navigation.state.params.title}` });
 
-    render() {
-        const { params } = this.props.navigation.state;
+  render() {
+    return (
+      <Container>
 
-        return (
-          <Container>
+        <Content>
+          <View style={[styles.layout]}>
+            <TouchableHighlight onPress={() => this._onPressButton('http://www.divaina.com')}>
+              <View style={styles.box}>
+                <ResponsiveImage source={require('./img/divaina.png')} initWidth="138" initHeight="138" />
+              </View>
+            </TouchableHighlight>
 
-              <Content>
-                <View style={[styles.layout]}>
-                    <TouchableHighlight onPress={() => this._onPressButton('http://www.divaina.com')}>
-                        <View style={styles.box} >
-                            <ResponsiveImage source={require('./img/divaina.png')} initWidth="138" initHeight="138"/>
-                        </View>
-                    </TouchableHighlight>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/lankadeepa.png')} initWidth="138" initHeight="138" />
+            </View>
+          </View>
 
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/lankadeepa.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                </View>
-
-                <View style={[styles.layout]}>
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/rivira.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                    <View style={styles.box} >
-                        <ResponsiveImage source={require('./img/derana.png')} initWidth="138" initHeight="138"/>
-                    </View>
-                </View>
-              </Content>
-          </Container>
-        );
-
-
-    }
+          <View style={[styles.layout]}>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/rivira.png')} initWidth="138" initHeight="138" />
+            </View>
+            <View style={styles.box}>
+              <ResponsiveImage source={require('./img/derana.png')} initWidth="138" initHeight="138" />
+            </View>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
 }
