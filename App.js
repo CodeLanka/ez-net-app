1792fcfb1fbf81aaab5f7ea39baf61d74e2a157e
Anusha Dharmasena <nushydude@gmail.com>
2017-07-09 16:22:17 +1000

    Mocking data fetch from an api end-point

--- App.js
+++ App.js
@@ -9,46 +9,13 @@ import PropTypes from 'prop-types';
 import TwoColumnView from './src/components/common/TwoColumnView';
 import BoxItem from './src/components/common/BoxItem';
 
-const categories = [
-  {
-    id: '1',
-    title: 'පුවත් පත්',
-    thumbnail: 'CATEGORY_NEWSPAPERS',
-    items: [
-      {
-        id: '1_1',
-        title: 'දිවයින',
-        thumbnail: 'NEWSPAPERS_DIVAINA',
-        url: 'http://www.divaina.com/',
-      },
-      {
-        id: '1_2',
-        title: 'ලංකාදීප',
-        thumbnail: 'NEWSPAPERS_LANKADEEPA',
-        url: 'http://www.lankadeepa.lk/',
-      },
-      {
-        id: '1_3',
-        title: 'රිවිර',
-        thumbnail: 'NEWSPAPERS_RIVIRA',
-        url: 'http://www.rivira.lk/online/',
-      },
-      {
-        id: '1_4',
-        title: 'දෙරණ',
-        thumbnail: 'NEWSPAPERS_DERANA',
-        url: 'http://www.adaderana.lk/',
-      },
-    ],
-  },
-];
+import { categories } from './src/api/dummy_api';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
-
 
 export default class ReactNativeProject extends Component {
   static propTypes = {
@@ -58,6 +25,19 @@ export default class ReactNativeProject extends Component {
   static navigationOptions = {
     title: 'අන්තර්ජාල නැබ',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      categories,
+    });
+  }
 
   renderCategories() {
     const { navigate } = this.props.navigation;
