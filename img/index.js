c53631806203705b7684af02c113ad50044ece43
Anusha Dharmasena <nushydude@gmail.com>
2017-07-09 12:04:11 +1000

    Create a file to export all the images

---
+++ img/index.js
@@ -0,0 +1,6 @@
+export const CATEGORY_NEWSPAPERS = require('./news-papers.png');
+
+export const NEWSPAPERS_DIVAINA = require('./divaina.png');
+export const NEWSPAPERS_LANKADEEPA = require('./lankadeepa.png');
+export const NEWSPAPERS_RIVIRA = require('./rivira.png');
+export const NEWSPAPERS_DERANA = require('./derana.png');
