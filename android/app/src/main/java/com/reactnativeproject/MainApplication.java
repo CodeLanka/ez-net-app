9aa38356a70fee2475cdacdcdc2784a997b3c546
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2017-07-09 13:06:41 +0530

    Add RNFirbase for Firebase intregation

--- android/app/src/main/java/com/reactnativeproject/MainApplication.java
+++ android/app/src/main/java/com/reactnativeproject/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.invertase.firebase.RNFirebasePackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNFirebasePackage(),
             new VectorIconsPackage()
       );
     }
