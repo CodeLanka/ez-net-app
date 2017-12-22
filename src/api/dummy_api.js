934afdb6d74bbf800e19f119bf0b5c51fdf644f7
Tharaka Devinda <tharaka@vista.aero>
2017-12-22 11:00:40 +0530

    added categories

--- src/api/dummy_api.js
+++ src/api/dummy_api.js
@@ -1,20 +1,20 @@
 export const categories = [
   {
     id: '1',
-    title: 'පුවත් පත්',
+    title: 'පත්තරේ විස්තරේ',
     thumbnail: 'CATEGORY_NEWSPAPERS',
     items: [
       {
         id: '1_1',
-        title: 'දිවයින',
-        thumbnail: 'NEWSPAPERS_DIVAINA',
-        url: 'http://www.divaina.com/',
-      },
-      {
-        id: '1_2',
         title: 'ලංකාදීප',
         thumbnail: 'NEWSPAPERS_LANKADEEPA',
         url: 'http://www.lankadeepa.lk/',
+      },
+      {
+        id: '1_2',
+        title: 'දිනමිණ',
+        thumbnail: 'NEWSPAPERS_DINAMINA',
+        url: 'http://www.dinamina.lk/',
       },
       {
         id: '1_3',
@@ -24,10 +24,139 @@ export const categories = [
       },
       {
         id: '1_4',
-        title: 'දෙරණ',
-        thumbnail: 'NEWSPAPERS_DERANA',
-        url: 'http://www.adaderana.lk/',
+        title: 'තරුණි',
+        thumbnail: 'NEWSPAPERS_THARUNIE',
+        url: 'http://www.Tharunie.lk/',
+      },
+      {
+        id: '1_5',
+        title: 'නවලිය',
+        thumbnail: 'NEWSPAPERS_NAWALIYA',
+        url: 'http://navaliya.com/',
       },
     ],
   },
+  {
+    id: '2',
+    title: 'කෑම වට්ටෝරු',
+    thumbnail: 'CATEGORY_RECIPIES',
+    items: [
+      {
+        id: '2_1',
+        title: 'කෑම වට්ටෝරු',
+        thumbnail: 'RECIPIES_YOUTUBE',
+        url: 'https://www.youtube.com/channel/UCVz1bXiY1pz6Fz_Osm0YYBA',
+      },
+    ],
+  },
+  {
+    id: '3',
+    title: 'මැහුම් ගෙතුම්',
+    thumbnail: 'CATEGORY_SEWING',
+    items: [
+      {
+        id: '3_1',
+        title: 'සාරී හැට්ට',
+        thumbnail: 'SEWING_BLOUSES',
+        url: 'https://www.youtube.com/results?search_query=saree+blouse+blocks',
+      },
+       {
+        id: '3_2',
+        title: 'ගවුම් මහන්න',
+        thumbnail: 'SEWING_GOWNS',
+        url: 'https://www.youtube.com/results?search_query=how+to+sew+a+dress',
+      },
+
+    ],
+  },
+  {
+    id: '4',
+    title: 'බණ දහම්',
+    thumbnail: 'CATEGORY_RELIG',
+    items: [
+      {
+        id: '4_1',
+        title: 'බණ දහම් හා අනෙකුත් ආගමික විස්තර',
+        thumbnail: 'RELIG_BANA',
+        url: 'https://www.youtube.com/results?search_query=bana',
+      },
+    ],
+  },
+  {
+    id: '5',
+    title: 'ළමා අධ්‍යාපන අත්වැල',
+    thumbnail: 'CATEGORY_EDUCATIONAL',
+    items: [
+      {
+        id: '5_1',
+        title: '5 වසර',
+        thumbnail: 'EDUCATIONAL_YR5',
+        url: 'https://www.youtube.com/watch?v=DUWKu6WSoA8&list=PL94ZZLMZi8sXa8CeaJlDZQ6JY2qc7CKiS',
+      },
+      {
+        id: '5_2',
+        title: 'e-තක්ෂලාව',
+        thumbnail: 'EDUCATIONAL_ETHAK',
+        url: 'http://www.e-thaksalawa.moe.gov.lk/',
+      },
+    ],
+  },
+  {
+    id: '6',
+    title: 'අඩු මිලට බඩු මිලදී ගන්න',
+    thumbnail: 'CATEGORY_ECOMM',
+    items: [
+      {
+        id: '6_1',
+        title: 'WOW.LK',
+        thumbnail: 'ECOMM_WOW',
+        url: 'http://www.wow.lk/',
+      },
+      {
+        id: '6_2',
+        title: 'TAKAS.LK',
+        thumbnail: 'ECOMM_TAKAS',
+        url: 'http://takas.lk/',
+      },
+      {
+        id: '6_3',
+        title: 'DARAZ.LK',
+        thumbnail: 'ECOMM_DARAZ',
+        url: 'https://www.daraz.lk/',
+      },
+    ],
+  },
+  {
+    id: '7',
+    title: 'විඩියෝ කෝල් ගැනීම',
+    thumbnail: 'CATEGORY_CALLS',
+    items: [
+      {
+        id: '7_1',
+        title: 'Skype',
+        thumbnail: 'CALLS_SKYPE',
+        url: 'https://play.google.com/store/apps/details?id=com.skype.raider',
+      },
+      {
+        id: '7_2',
+        title: 'Imo',
+        thumbnail: 'CALLS_IMO',
+        url: 'https://play.google.com/store/apps/details?id=com.imo.android.imoim&hl=en',
+      },
+    ],
+  },
+  {
+    id: '8',
+    title: 'ගීත අසන්න',
+    thumbnail: 'CATEGORY_SONGS',
+    items: [
+      {
+        id: '8_1',
+        title: 'සිංහල සින්දු',
+        thumbnail: 'SONGS_SINHALA',
+        url: 'https://www.youtube.com/results?search_query=sinhala+songs',
+      },
+    ],
+  },
+
 ];
