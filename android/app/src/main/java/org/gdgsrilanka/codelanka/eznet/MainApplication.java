52d1e334f599ee9ae331830c58566b214f6ba9cc
Moses Paul R <iammosespaulr@gmail.com>
2018-01-11 17:28:49 +0530

    Update MainApplication.java

--- android/app/src/main/java/org/gdgsrilanka/codelanka/eznet/MainApplication.java
+++ android/app/src/main/java/org/gdgsrilanka/codelanka/eznet/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
+import com.react.rnspinkit.RNSpinkitPackage;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
